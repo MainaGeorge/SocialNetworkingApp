b77ec73a86d3412cfd6df6a42a40ac600e2243c4
MainaGeorge <nuriszka@gmail.com>
2021-10-19 15:20:16 +0200

    Added a walking skeleton of the client/angular app

--- API/Startup.cs
+++ API/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Data;
+using API.Entities;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -38,6 +39,8 @@ namespace API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
             });
+
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -53,6 +56,10 @@ namespace API
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseCors(policy => policy.AllowAnyHeader()
+                .AllowAnyMethod()
+                .WithOrigins("https://localhost:4200"));
 
             app.UseAuthorization();
 
---
+++ Client/src/app/app.component.ts
@@ -0,0 +1,25 @@
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit{
+  title = 'Client';
+  users: any;
+
+  constructor(private http:HttpClient) {
+  }
+
+
+  ngOnInit(): void {
+    this.getUsers();
+  }
+
+  getUsers() {
+    this.http.get("https://localhost:5001/api/users")
+      .subscribe( users => this.users = users, err => console.log(err));
+  }
+}
